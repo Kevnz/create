377d1483125073f293bff97f7b2fe575a4d040b1
Kevin Isom <kevin@isom.nz>
2019-04-09 21:51:56 +1200

    dependencies

---
+++ src/dependencies/base.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dependencies: [],
+  devDependencies: ['coveralls', 'eslint', '@kev_nz/eslint-config', 'jest'],
+}
---
+++ src/dependencies/deck.js
@@ -0,0 +1,4 @@
+module.exports = {
+  dependencies: [],
+  devDependencies: ['mdx-deck', 'mdx-deck-code-surfer', 'raw-loader'],
+}

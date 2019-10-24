c8f31b2e2b159fb6e07408b02111164463958d3f
Kevin Isom <kevin.isom@gmail.com>
2019-10-25 08:34:36 +1300

    serverless tasks and deps

---
+++ src/dependencies/serverless.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dependencies: [
+    '@hapi/hapi',
+    '@hapi/vision',
+    '@hapi/inert',
+    '@hapi/good',
+    '@hapi/good-console',
+    'blipp',
+    'bookshelf',
+    'hapi-router',
+    'jsonwebtoken',
+    'xtconf',
+    'lalalambda',
+  ],
+  devDependencies: ['nodemon', 'serverless', 'serverless-offline'],
+}
